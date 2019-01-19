fec59b8761d726ffa062719a5a82ecd7ca6694f9
Ryan <falcsmike7@gmail.com>
2019-01-19 11:03:32 -0500

    added conditionals for summer/winter and css styling

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,4 +1,11 @@
+import './SeasonDisplay.css';
 import React from 'react';
+
+const seasonConfig = {
+  summer: { text: 'Lets hit the beach!', iconName: 'sun' },
+  winter: { text: 'Burr it is cold!', iconName: 'snowflake' }
+}
+
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
     return lat > 0 ? 'summer' : 'winter';
@@ -8,7 +15,15 @@ const getSeason = (lat, month) => {
 }
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth())
-  return <div><h1>{season === 'winter' ? 'Burr, it is chilly' : 'Lets hit the beach!'}</h1></div>
+  const { text, iconName } = seasonConfig[season]
+
+  return (
+    <div className={`season-display ${season}`}>
+      <i className={`icon-left massive ${iconName} icon`} />
+      <h1>{text}</h1>
+      <i className={`icon-right massive ${iconName} icon`} />
+    </div>
+  )
 }
 
 export default SeasonDisplay;
